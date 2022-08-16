f97e9419b3eca414347cf957861c3fd2257b0ee1
Iqrah Nadeem <iiqrah.dev@gmail.com>
2022-08-17 00:09:59 +0300

    Configure wagmi chain, connector and client variables

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,27 @@ import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
+// 1. Confugure chain
+const alchemyID = process.env.NEXT_PUBLIC_ALCHEMY_ID;
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon],
+  [alchemyProvider({ apiKey: alchemyID })]
+);
+
+// 2. Confugure connector
+const { connectors } = getDefaultWallets({
+  appName: "web3rsvp",
+  chains,
+});
+
+// 3. Confugure client
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
